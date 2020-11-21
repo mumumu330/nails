abbb30f0bcdf2ef5e777bc50249945ac0d8cf6a3
mumumu330 <ritz30akn@gmail.com>
2020-11-22 01:32:56 +0900

    add user_spec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,5 +18,24 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors[:name]).to include("is too long (maximum is 10 characters)")
     end
+
+    it "メールアドレスがなければ無効な状態であること" do
+      @user.email = ""
+      @user.valid?
+      expect(@user.errors[:email]).to include("can't be blank")
+    end
+
+    it "パスワードが6文字以上でなければ無効であること" do
+      @user.password = "a" * 5
+      @user.valid?
+      expect(@user.errors[:password]).to include("is too short (minimum is 6 characters)")
+    end
+
+    # it "重複したメールアドレスなら無効な状態であること" do
+    #   FactoryBot.create(:user)
+    #   @user.valid?
+    #   expect(@user.errors[:email]).to include("already been taken")
+    # end
+
   end
 end
